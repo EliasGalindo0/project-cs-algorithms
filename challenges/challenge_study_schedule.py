aae7f87f2e4f3d8a1bb1e01b9ed3db01d83ec8e1
Elias <eliasgalindo@gmail.com>
2022-11-22 14:31:45 -0300

    feat: chanllenge_study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or not isinstance(target_time, int):
+        return None
+
+    counter = 0
+    for permanence in permanence_period:
+        entry_time, exit_time = permanence
+        if not isinstance(entry_time, int) or not isinstance(exit_time, int):
+            return None
+        if entry_time <= target_time <= exit_time:
+            counter += 1
+    return counter
