bd711e8d80da17d525608d6d28260b8ac390f885
Elias <eliasgalindo@gmail.com>
2022-11-23 17:00:11 -0300

    feat:  test_encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    with pytest.raises(TypeError) as exc_message:
+        encrypt_message(15, 5)
+    assert exc_message.value.args[0] == "tipo inválido para message"
+    with pytest.raises(TypeError)as exc_key:
+        encrypt_message("AABBCC", "4")
+
+    assert exc_key.value.args[0] == "tipo inválido para key"
